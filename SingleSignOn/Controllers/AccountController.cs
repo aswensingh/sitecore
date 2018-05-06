d5058541698b10c75c224c918af7576df3e9eabb
aswensingh <aswen.lp@gmail.com>
2018-05-07 01:52:56 +0800

    Initial commit

---
+++ SingleSignOn/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace SingleSignOn.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            //if (FormsAuthentication.Authenticate(username, password))
+            if (ValidateUser(username, password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login details");
+                ViewBag.ReturnUrl = returnUrl;
+                return View();
+            }
+        }
+
+        private bool ValidateUser(string userName, string passWord)
+        {
+            SqlConnection conn;
+            SqlCommand cmd;
+            string lookupPassword = null;
+            
+            try
+            {
+                // Consult with your SQL Server administrator for an appropriate connection
+                // string to use to connect to your local SQL Server.
+                //conn = new SqlConnection("server=.\\SQLEXPRESS;Integrated Security=SSPI;database=Sitecore");
+
+                //string connectionString = @"Data Source=.\SQLEXPRESS; AttachDbFilename=|DataDirectory|\Sitecore.mdf; Integrated Security=True;Connect Timeout=30;User Instance=True";
+
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnStringDb"].ConnectionString;
+
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnStringDb"].ConnectionString);
+
+                conn.Open();
+
+                // Create SqlCommand to select pwd field from users table given supplied userName.
+                cmd = new SqlCommand("Select pwd from users where uname=@userName", conn);
+                cmd.Parameters.Add("@userName", SqlDbType.VarChar, 25);
+                cmd.Parameters["@userName"].Value = userName;
+
+                // Execute command and fetch pwd field into lookupPassword string.
+                lookupPassword = (string)cmd.ExecuteScalar();
+
+                // Cleanup command and connection objects.
+                cmd.Dispose();
+                conn.Dispose();
+            }
+            catch (Exception ex)
+            {
+                // Add error handling here for debugging.
+                // This error message should not be sent back to the caller.
+                System.Diagnostics.Trace.WriteLine("[ValidateUser] Exception " + ex.Message);
+            }
+
+            // If no password found, return false.
+            if (null == lookupPassword)
+            {
+                // You could write failed login attempts here to event log for additional security.
+                return false;
+            }
+
+            // Compare lookupPassword and input passWord, using a case-sensitive comparison.
+            return (0 == string.Compare(lookupPassword, passWord, false));
+
+        }
+
+    }
+}
